459636e87b9d38a8dc602222df7c3e4e39af2aad
anthu1510 <anthu1510@gmail.com>
2019-06-16 17:45:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = { 
+        count: '15-10-92'
+     }
+     
+    render() { 
+        return ( 
+            <h1>React App Parcel Js...</h1>
+         );
+    }
+}
+ 
+export default App;
